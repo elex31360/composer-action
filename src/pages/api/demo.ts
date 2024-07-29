76683511d48b07bd9ed2badde2a7e6c735de4adb
elex31360 <168320842+elex31360@users.noreply.github.com>
2024-07-29 16:02:59 +0800

    first

---
+++ src/pages/api/demo.ts
@@ -0,0 +1,67 @@
+import { env } from "@/env";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { NeynarAPIClient } from "@neynar/nodejs-sdk";
+import { encryptFid } from "@/utils/token";
+
+const neynarClient = new NeynarAPIClient(env.NEYNAR_API_KEY);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const data = req.body as {
+      untrustedData: {
+        fid: number;
+        url: string;
+        messageHash: string;
+        timestamp: number;
+        network: number;
+        buttonIndex: number;
+        state: string;
+      };
+      trustedData: {
+        messageBytes: string;
+      };
+    };
+    const messageBytes = data.trustedData.messageBytes;
+    const result = await neynarClient.validateFrameAction(messageBytes);
+    if (result.valid) {
+      const timestamp = Math.floor(
+        new Date(result.action.timestamp).getTime() / 1000
+      );
+      if (timestamp > new Date().getTime() / 1000 - 60) {
+        const token = encryptFid(result.action.interactor.fid);
+        // const stateObject = decodeURIComponent(data.untrustedData.state);
+        // const t = JSON.parse(stateObject) as {
+        //   cast: {
+        //     text: string;
+        //   };
+        // };
+        // const stateString = JSON.stringify(t.cast);
+        // console.log(stateString);
+        res.status(200).json({
+          type: "form",
+          title: "Cast AI Editor",
+          url: `https://apps.recaster.org/editor?token=${token}&state=${data.untrustedData.state}`, // save to db? maybe too long?
+        });
+        return;
+      }
+    }
+    res.status(401);
+  } else {
+    res.status(200).json({
+      type: "composer",
+      name: "Cast AI Editor",
+      icon: "pencil",
+      description: "Use AI to help you create cast",
+      aboutUrl: "https://apps.recaster.org/",
+      imageUrl: "https://apps.recaster.org/_images/editor.png",
+      action: {
+        type: "post",
+      },
+    });
+  }
+}
