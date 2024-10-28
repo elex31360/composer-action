4671a33f1aff0a2517b39f15440689d21773e883
elex31360 <168320842+elex31360@users.noreply.github.com>
2024-10-28 16:22:50 +0800

    fix

--- src/pages/api/demo.ts
+++ src/pages/api/demo.ts
@@ -13,59 +13,59 @@ export default async function handler(
 ) {
   console.log(neynarClient, "neynarClient");
   console.log(req.method, "req");
-  // if (req.method === "POST") {
-  const data = req.body as {
-    untrustedData: {
-      fid: number;
-      url: string;
-      messageHash: string;
-      timestamp: number;
-      network: number;
-      buttonIndex: number;
-      state: string;
+  if (req.method === "POST") {
+    const data = req.body as {
+      untrustedData: {
+        fid: number;
+        url: string;
+        messageHash: string;
+        timestamp: number;
+        network: number;
+        buttonIndex: number;
+        state: string;
+      };
+      trustedData: {
+        messageBytes: string;
+      };
     };
-    trustedData: {
-      messageBytes: string;
-    };
-  };
-  console.log(data, "data");
+    console.log(data, "data");
 
-  // const messageBytes = data.trustedData.messageBytes;
-  // const result = await neynarClient.validateFrameAction(messageBytes);
-  // if (result.valid) {
-  //   const timestamp = Math.floor(
-  //     new Date(result.action.timestamp).getTime() / 1000
-  //   );
-  //   if (timestamp > new Date().getTime() / 1000 - 60) {
-  //     const token = encryptFid(result.action.interactor.fid);
-  // const stateObject = decodeURIComponent(data.untrustedData.state);
-  // const t = JSON.parse(stateObject) as {
-  //   cast: {
-  //     text: string;
-  //   };
-  // };
-  // const stateString = JSON.stringify(t.cast);
-  // console.log(stateString);
-  res.status(200).json({
-    type: "form",
-    title: "Cast AI Editor",
-    url: `https://composer-action.vercel.app/`, // save to db? maybe too long?
-  });
-  //       return;
-  //     }
-  //   }
-  //   res.status(401);
-  // } else {
-  // res.status(200).json({
-  //   type: "composer",
-  //   name: "Cast AI Editor",
-  //   icon: "pencil",
-  //   description: "Use AI to help you create cast",
-  //   aboutUrl: "https://composer-action.vercel.app/",
-  //   imageUrl: "https://composer-action.vercel.app/bear.png",
-  //   action: {
-  //     type: "post",
-  //   },
-  // });
-  // }
+    // const messageBytes = data.trustedData.messageBytes;
+    // const result = await neynarClient.validateFrameAction(messageBytes);
+    // if (result.valid) {
+    //   const timestamp = Math.floor(
+    //     new Date(result.action.timestamp).getTime() / 1000
+    //   );
+    //   if (timestamp > new Date().getTime() / 1000 - 60) {
+    //     const token = encryptFid(result.action.interactor.fid);
+    // const stateObject = decodeURIComponent(data.untrustedData.state);
+    // const t = JSON.parse(stateObject) as {
+    //   cast: {
+    //     text: string;
+    //   };
+    // };
+    // const stateString = JSON.stringify(t.cast);
+    // console.log(stateString);
+    res.status(200).json({
+      type: "form",
+      title: "Cast AI Editor",
+      url: `http://localhost:4001/Home`, // save to db? maybe too long?
+    });
+    //       return;
+    //     }
+    //   }
+    //   res.status(401);
+    // } else {
+    // res.status(200).json({
+    //   type: "composer",
+    //   name: "Cast AI Editor",
+    //   icon: "pencil",
+    //   description: "Use AI to help you create cast",
+    //   aboutUrl: "https://composer-action.vercel.app/",
+    //   imageUrl: "https://composer-action.vercel.app/bear.png",
+    //   action: {
+    //     type: "post",
+    //   },
+    // });
+  }
 }
